b95b4c90afecc1905b613f2d2a0f43591a0075e4
minc_nice_100 <100515973+minc-nice-100@users.noreply.github.com>
2025-03-30 00:46:15 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,18 +2,18 @@
 window.Config = {
 
   // 显示标题
-  SiteName: '云生站点监测',
+  SiteName: 'Ited Blog Status Page',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur1078385-b9802a0868414b8a975d5038',
+    'ur2076229-8e5966f49b9811365ce2fe89',
   ],
 
     // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 90,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -21,16 +21,12 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: '主页',
-      url: 'https://tjys.tk'
+      text: 'Home',
+      url: '/'
     },
     {
-      text: '博客',
-      url: 'https://blog.qikaile.tk'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/qikaile/uptime-status'
-    },
+      text: 'Blog',
+      url: 'https://itedev.com/'
+    }
   ],
 };
