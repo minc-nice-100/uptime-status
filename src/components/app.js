9c068ecd7010817e1bdedbb7d3a42fe2360d4e56
minc_nice_100 <100515973+minc-nice-100@users.noreply.github.com>
2025-03-30 12:35:12 +0800

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -22,25 +22,9 @@ function App() {
             <UptimeRobot key={key} apikey={key} />
           ))}
         </div>
-          <script>
-              function show_hitokoto() {
-        var xhr = new XMLHttpRequest();
-        xhr.open('get', 'https://hitokoto.itedev.com/');
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-                const data = JSON.parse(xhr.responseText);
-                hitokoto.innerText = data.hitokoto;
-            }
-        }
-        xhr.send();
-    };
-        show_hitokoto();
 
-          </script>
         <div id='footer'>       
-            <a href="javascript:void(0);" onclick="show_hitokoto()" title="点击刷新"><b>
-                <div id="hitokoto">Loading...</div>
-            </b></a>
+          <p><b>无论如何, 你已经很棒了!</b></p>
           <p>Copr. 2025 Ited Blog</p>
         </div>
       </div>
