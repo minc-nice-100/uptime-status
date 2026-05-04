99ec401b18cd481d0cbae493db864000d29a3373
minc-nice-100 <minc-nice-100@users.noreply.github.com>
2026-05-04 08:08:02 +0000

    deploy: 14942ef80dde2c42e403e72e8143e881c64f64aa

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ window.Config = {
     // Log days
   // Although the free version claims to save logs for only 60 days, testing shows API can retrieve 90 days
   // However, don't set the time too long as it may cause lag and API requests may fail
-  CountDays: 90,
+  CountDays: 30,
 
   // Whether to show links to monitored sites
   ShowLink: true,
